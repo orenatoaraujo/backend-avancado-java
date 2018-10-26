64d968c1959ca02380fef4a704c29df9c1a5a12a
danylo-macelai <danylomacelai@gmail.com>
2018-10-26 16:52:21 -0200

    Utilizado o RepositoryRestResource para construir os serviços

--- src/main/java/br/inf/ufg/bea/repository/ContatoRepository.java
+++ src/main/java/br/inf/ufg/bea/repository/ContatoRepository.java
@@ -1,9 +1,13 @@
 package br.inf.ufg.bea.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import br.inf.ufg.bea.model.ContatoTO;
 
+@CrossOrigin
+@RepositoryRestResource(path="/contatos")
 public interface ContatoRepository extends CrudRepository<ContatoTO, Long> {
 
 }
